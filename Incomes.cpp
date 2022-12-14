6a9ed2aa48f5c1100752f07a561c0eca12b989ca
Radosław Wawryczuk <radoslaw.wawryczuk@gmail.com>
2022-12-14 09:18:49 +0100

    incomes class made

--- Incomes.cpp
+++ Incomes.cpp
@@ -1,1 +1,35 @@
 #include "Incomes.h"
+
+int Incomes::getIncomeId()
+{
+    return incomeId; 
+}
+int Incomes::getAmount()
+{
+    return amount; 
+}
+string Incomes::getDate()
+{
+    return date; 
+}
+string Incomes::getItem()
+{
+    return item; 
+}
+
+void Incomes::setIncomeId(int INCOMEID)
+{
+    incomeId = INCOMEID; 
+}
+void Incomes::setAmount(int AMOUNT)
+{
+    amount = AMOUNT; 
+}
+void Incomes::setDate(string DATE)
+{
+    date = DATE; 
+}
+void Incomes::setItem(string ITEM)
+{
+    item = ITEM; 
+}
--- Incomes.h
+++ Incomes.h
@@ -2,7 +2,24 @@
 #define INCOMES_H
 #include<iostream>
 
+using namespace std; 
 
+class Incomes
+{
+    int incomeId, amount; 
+    string date, item; 
+
+public: 
+    int getIncomeId();
+    int getAmount();
+    string getDate();
+    string getItem();
+
+    void setIncomeId(int INCOMEID);
+    void setAmount(int AMOUNT);
+    void setDate(string DATE);
+    void setItem(string ITEM); 
+};
 
 
 
